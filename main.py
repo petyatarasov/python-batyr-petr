aefa8158426a839b39f735650d6dc0531f7e77c2
petyatarasov <112955136+petyatarasov@users.noreply.github.com>
2022-09-14 18:31:38 +0600

    добавил models.py

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ if __name__ == "__main__":
     filename = "hello.jpg"
     result = get_filename(filename)
     print(result)
+
+name = 'Shaya'
+print('hi, ' + name.upper() + '!')  # hi, SHAYA!
+
+text = 'Never forget what you are, for surely the world will not'
+
+# BEGIN (write your solution here)
+print(f"Index Of N: {text.find('N')}\nIndex Of ,: {text.find(',')}")
+# END
+
+text = 'When \t\n you play a \t\n game of thrones you win or you die.'
+
+# BEGIN (write your solution here)
+print(len(text[5:15].strip()))
+# END
---
+++ models.py
@@ -0,0 +1,19 @@
+from peewee import *
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+DATABASE_NAME = env("DATABASE_NAME")
+DATABASE_DSN = env("DATABASE_DSN")
+
+
+db = PostgresqlDatabase(
+    database=DATABASE_NAME,
+    dsn=DATABASE_DSN,
+)
+
+
+if __name__ == "__main__":
+    db.connect()
